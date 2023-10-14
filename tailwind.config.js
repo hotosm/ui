4c1efc10f1d7a35f6c10df57fb3ae463b59cd08c
JoltCode <joe@joecc.dev>
2023-10-14 12:00:35 +0100

    feat,chore: init tailwindcss, ladle, react, typescript

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        "DEFAULT": "0.125rem"
+      },
+      colors: {
+        "primary": "#d73f3f"
+      }
+    },
+  },
+  plugins: [],
+}
