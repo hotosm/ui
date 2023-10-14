9313bcf3db6023cc834ef535184624d6deabe6af
JoltCode <joe@joecc.dev>
2023-10-14 17:37:38 +0100

    feat(components): init card

---
+++ src/card/Card.stories.tsx
@@ -0,0 +1,15 @@
+import {Card} from "./Card";
+
+export const CardStory = () => (
+  <Card>
+    <Card.CardTitle>
+      Card Title
+    </Card.CardTitle>
+    <Card.CardBody>
+      lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+    </Card.CardBody>
+    <Card.CardFooter>
+      Card Footer
+    </Card.CardFooter>
+  </Card>
+);
---
+++ src/card/Card.tsx
@@ -0,0 +1,64 @@
+import {cn} from "../utils/merge";
+
+export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const Card = (props: CardProps) => {
+  const {className, ...rest} = props;
+
+  return (
+    <div
+      className={cn("bg-white rounded shadow-md p-6 flex flex-col", className)}
+      {...rest}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+interface CardTitleProps extends React.HTMLAttributes<HTMLHeadingElement> {}
+const CardTitle = (props: CardTitleProps) => {
+  const {className, ...rest} = props;
+
+  return (
+    <h2
+      className={cn("text-2xl font-bold", className)}
+      {...rest}
+    >
+      {props.children}
+    </h2>
+  )
+}
+
+Card.CardTitle = CardTitle;
+
+interface CardBodyProps extends React.HTMLAttributes<HTMLDivElement> {}
+const CardBody = (props: CardBodyProps) => {
+  const {className, ...rest} = props;
+
+  return (
+    <div
+      className={cn("mt-4 grow", className)}
+      {...rest}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+Card.CardBody = CardBody;
+
+interface CardFooterProps extends React.HTMLAttributes<HTMLDivElement> {}
+const CardFooter = (props: CardFooterProps) => {
+  const {className, ...rest} = props;
+
+  return (
+    <div
+      className={cn("mt-4", className)}
+      {...rest}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+Card.CardFooter = CardFooter;
