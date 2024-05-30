8646bb0181503cd07faa69b333995c60b5fa27c0
spwoodcock <sam.woodcock@protonmail.com>
2024-05-30 10:35:50 +0100

    feat: add index.ts files for importing all components at once

---
+++ react/index.ts
@@ -0,0 +1,6 @@
+// Index to import all components together
+// import '@hotosm/ui/react';
+
+export { default as Button } from './Button';
+export { default as Toolbar } from './Toolbar';
+export { default as Tracking } from './Tracking';
