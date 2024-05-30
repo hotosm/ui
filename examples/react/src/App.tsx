424cc7526c642b823be1fd42c4285058f297b367
spwoodcock <sam.woodcock@protonmail.com>
2024-05-30 10:17:22 +0100

    refactor: rename HotElement --> Element for react

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import '@hotosm/ui/theme/hot.css';
 import hotLogo from '@hotosm/ui/theme/logo.png';
-import Toolbar from '@hotosm/ui/react/HotToolbar';
+import Toolbar from '@hotosm/ui/react/Toolbar';
 
 const App = () => {
   const [undoCount, setUndoCount] = useState(0);
---
+++ react/Button.tsx
@@ -0,0 +1,17 @@
+
+// @ts-ignore
+import * as React from 'react';
+import { createComponent } from '@lit/react';
+import Button from '../components/Button';
+
+const reactWrapper = createComponent({
+  tagName: 'hot-button',
+  elementClass: Button,
+  react: React,
+  events: {
+    onClick: 'click',
+  },
+  displayName: 'hot-button'
+});
+
+export default reactWrapper;
