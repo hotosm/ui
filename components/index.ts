dceb129a6c615feb02aea03b8e2ceb7be61b9de1
spwoodcock <sam.woodcock@protonmail.com>
2024-05-31 11:22:14 +0100

    build: use setBasePath in components index.ts not react

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,11 @@
 // Index to import all components together
 // import '@hotosm/ui/components';
 
+// setBasePath makes the icons available via CDN, instead of expecting them to be
+// on the webserver the runs the web-component
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+setBasePath('https://cdn.jsdelivr.net/npm/@hotosm/ui@latest/dist');
+
 export { default as Button } from './Button';
 export { default as Header } from './Header';
 export { default as Toolbar } from './Toolbar';
--- react/index.ts
+++ react/index.ts
@@ -1,11 +1,6 @@
 // Index to import all components together
 // import '@hotosm/ui/react';
 
-// setBasePath makes the icons available via CDN, instead of expecting them to be
-// on the webserver the runs the web-component
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
-setBasePath('https://cdn.jsdelivr.net/npm/@hotosm/ui@latest/dist');
-
 export { default as Button } from './Button';
 export { default as Header } from './Header';
 export { default as Toolbar } from './Toolbar';
