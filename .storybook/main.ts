6b2eede8d87d5a1f3f0a4fa7358bee43330f4a2e
spwoodcock <sam.woodcock@protonmail.com>
2024-06-03 00:03:57 +0100

    fix: bundle missing shoelace icons with storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from "@storybook/web-components-vite";
 
 const config: StorybookConfig = {
   stories: ["./stories/**/*.stories.ts"],
+  staticDirs: ["./static"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: {
     name: "@storybook/web-components-vite",
