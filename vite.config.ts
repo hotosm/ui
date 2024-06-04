7869cf549f761db9ea93b15e993e84481c989532
spwoodcock <sam.woodcock@protonmail.com>
2024-06-04 20:27:24 +0100

    build: replace rollup with vite

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    // minify: false,
+    lib: {
+      entry: 'components/index.ts',
+      formats: ['es'],
+      fileName:  'components',
+    },
+    rollupOptions: {
+      external: /^lit/,
+    },
+  },
+  plugins: [
+    UnoCSS({
+      mode: 'shadow-dom',
+    }),
+  ],
+})
