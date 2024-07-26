ead1d2dd02aeb8804ec8b0a18621a71bb8e6487b
JoltCode <joe@joecc.dev>
2024-07-26 14:43:32 +0100

    feat(exports): export .d.ts files on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite'
-import UnoCSS from 'unocss/vite'
+import UnoCSS from "unocss/vite";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     // minify: false,
     lib: {
-      entry: 'components/index.ts',
-      formats: ['es'],
-      fileName:  'components',
+      entry: "components/index.ts",
+      formats: ["es"],
+      fileName: "components",
     },
     rollupOptions: {},
   },
   plugins: [
     UnoCSS({
-      mode: 'shadow-dom',
+      mode: "shadow-dom",
     }),
+    dts(),
   ],
-})
+});
