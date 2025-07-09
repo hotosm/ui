6e2d31b7beb5812958f8cb3a3746d25b79f55e01
Deepak Pradhan <deepak.pradhan048@gmail.com>
2025-07-09 14:51:33 -0500

    feat: fix styling for header button

--- dist/chunks/chunk.AP2LRWVP.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./chunk.AUBRGELY.js";var r=e;customElements.define("hot-header",e);export{r as a};
---
+++ dist/chunks/chunk.CNARC7G7.js
@@ -0,0 +1,1 @@
+import{b as e}from"./chunk.HCXPZJU6.js";var r=e;customElements.define("hot-header",e);export{r as a};
